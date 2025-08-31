2ee4dfc7d4e95443f95e3f0aa090adda2992f492
Ishani poojary <ishanipoojary131@gmail.com>
2025-08-31 18:14:42 +0530

    Uploaded Campus Courier Lab Program

---
+++ SecurityRules.java
@@ -0,0 +1,17 @@
+package transport;
+
+public final class SecurityRules {
+
+	public SecurityRules() {}
+		public static boolean canFly (String place) {
+			  if (place == "ExamCell")
+				  {
+				  return true;
+		     	  }
+			  else {
+			  return false;
+
+	}
+		}
+}
+		
